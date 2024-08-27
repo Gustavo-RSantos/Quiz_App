64e07e5e53b05fb24eaf44c5063f687a5f81a81c
Gustavo-RSantos <144718713+Gustavo-RSantos@users.noreply.github.com>
2024-08-27 17:57:40 -0300

    Movendo a logica de alguns componentes
    Modificação da logica de alguns componentes para que o dado seja usado apenas onde necessário

--- src/components/Answers/Answer.jsx
+++ src/components/Answers/Answer.jsx
@@ -33,6 +33,7 @@ export default function Answer({
             <button
               onClick={() => onSelect(answer)}
               className={cssClass}
+              disabled={answerState !== ''}
             >
               {answer}
             </button>
--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,26 +1,57 @@
 import QuestionTimer from "../QuestionTimer/QuestionTimer"
 import Answer from "../Answers/Answer"
+import { useState } from "react"
+import QUESTIONS from "../../questions"
 
 export default function Question({
-  questionText,
-  answers,
+  index,
   onSelectAnswer,
-  selectedAnswer,
-  answerState,
   onSkipAnswer
 }) {
+
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null,
+  })
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: QUESTIONS[index].answers[0] === answer,
+    })
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: true
+      })
+
+      setTimeout(() => {
+        onSelectAnswer(answer)
+      }, 2000)
+    }, 1000)
+  }
+
+  let answerState = '';
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'incorrect';
+  } else if (answer.selectedAnswer) {
+    answerState = 'selected';
+  }
+
   return (
     <div id="question">
       <QuestionTimer
         timeout={10000}
         onTimeout={onSkipAnswer}
       />
-      <h2>{questionText}</h2>
+      <h2>{QUESTIONS[index].text}</h2>
       <Answer
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
       />
     </div>
   )
